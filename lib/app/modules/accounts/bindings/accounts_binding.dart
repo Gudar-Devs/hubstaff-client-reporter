51de66e048222e3eef9111a0af5f5f5b75328cfa
Team GudarDevs 3 <teamgudardevs3@MacBook-Pro-de-Team.local>
2022-10-31 17:01:21 -0500

    accounts view added

---
+++ lib/app/modules/accounts/bindings/accounts_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import '../controllers/accounts_controller.dart';
+
+class AccountsBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<AccountsController>(
+      () => AccountsController(),
+    );
+  }
+}
